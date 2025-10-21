176fd844dcb9d894f38b761a9c7224c28b6d7b00
Abrar ul Riyaz <abrar_2022bite057@nitsri.ac.in>
2025-10-21 22:36:15 +0530

    update

--- Pydantic/06_nested_models.py
+++ Pydantic/06_nested_models.py
@@ -21,3 +21,15 @@ patient_dict={"name":"john","gender":"shemale","age":"40","address":address1}
 
 patient1=Patient(**patient_dict)
 print(patient1.address.pin)
+
+
+temp1=patient1.model_dump()
+temp2=patient1.model_dump(include=['name','gender']) #exclude as well exclude=[name,gender]
+temp3=patient1.model_dump(exclude={'address':['state']}) #exclude as well exclude=[name,gender]
+temp4=patient1.model_dump_json()
+
+#exclude unset
+
+
+print(temp3)
+print(type(temp2))
